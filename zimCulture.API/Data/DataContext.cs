cdd085de249d564d3371f4203b268cb5c0306e67
nicholas chikore <nickchikore@gmail.com>
2020-06-23 17:58:09 +0100

    first commit

---
+++ zimCulture.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using zimCulture.API.Model;
+
+namespace zimCulture.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ zimCulture.API/Model/Value.cs
@@ -0,0 +1,8 @@
+namespace zimCulture.API.Model
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
